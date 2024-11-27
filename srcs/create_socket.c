b9b7f63b01333be2e79317985ec9e50f6dee96a4
cjsauvain <cjsauvain@gmail.com>
2024-11-27 19:38:30 +0100

    filter only echo reply

--- srcs/create_socket.c
+++ srcs/create_socket.c
@@ -7,14 +7,14 @@ static void	set_sock_opt(int fd_socket)
 
 	tv_exit.tv_sec = 1;
 	tv_exit.tv_usec = 0;
-	if (setsockopt(fd_socket, SOL_SOCKET, SO_RCVTIMEO, &tv_exit, sizeof(tv_exit) == -1))
+	if (setsockopt(fd_socket, SOL_SOCKET, SO_RCVTIMEO, &tv_exit, sizeof(tv_exit)) == -1)
 	{
 		fprintf(stderr, "Could not set socket options...\n");
 		exit(1);
 	}
 
-	filter.data = 8;
-	if (setsockopt(fd_socket, SOL_SOCKET, SO_RCVTIMEO, &filter, sizeof(filter) == -1))
+	filter.data = ~(1 << ICMP_ECHOREPLY);
+	if (setsockopt(fd_socket, SOL_RAW, ICMP_FILTER, &filter, sizeof(filter)) == -1)
 	{
 		fprintf(stderr, "Could not set socket options...\n");
 		exit(1);
--- srcs/receive_echo_reply.c
+++ srcs/receive_echo_reply.c
@@ -4,7 +4,7 @@ void	receive_echo_reply(int fd_socket, struct sockaddr_in *addr)
 {
 	socklen_t		addr_len;
 	char			buffer[1500];
-	//struct icmphdr	*icmp_reply;
+	struct icmphdr	*icmp_reply;
 
 	addr_len = sizeof(addr);
 	if (recvfrom(fd_socket, buffer, 1500, 0, (struct sockaddr *)addr, &addr_len) == -1)
@@ -12,21 +12,7 @@ void	receive_echo_reply(int fd_socket, struct sockaddr_in *addr)
 		fprintf(stderr, "Could not receive packet...\n");
 		exit(1);
 	}
-	if (recvfrom(fd_socket, buffer, 1500, 0, (struct sockaddr *)addr, &addr_len) == -1)
-	{
-		fprintf(stderr, "Could not receive packet...\n");
-		exit(1);
-	}
-	/*
-	icmp_reply = (struct icmphdr *)(buffer);
-	printf("type = %02x\n", icmp_reply->type);
-	printf("code = %02x\n", icmp_reply->code);
-	printf("checksum = %04x\n", ntohs(icmp_reply->checksum));*/
-	for (int i = 1; i <= 76; i++)
-	{
-		printf("%02x ", (unsigned char)buffer[i - 1]);
-		if (!(i % 16))
-			printf("\n");
-	}
-	printf("\n");
+	icmp_reply = (struct icmphdr *)(buffer + 20);
+	if (icmp_reply->un.echo.id == 0)
+		return ;
 }
