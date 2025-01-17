e0daaadb2d48c0b596ac038e6a3e7878602ac751
cjsauvain <cjsauvain@gmail.com>
2025-01-17 11:47:29 -0600

    fix altered rtt when a packet isn't received

--- srcs/receiving/receive_echo_reply.c
+++ srcs/receiving/receive_echo_reply.c
@@ -11,7 +11,6 @@ static int	recv_ip_pckt(t_ping *ping)
 {
 	char			buffer[BUFFER_SIZE];
 	ssize_t			bytes_received;
-	int				status;
 
 	memset(buffer, 0, BUFFER_SIZE);
 	bytes_received = recv(ping->recv_socket, buffer, BUFFER_SIZE, 0);
@@ -21,10 +20,6 @@ static int	recv_ip_pckt(t_ping *ping)
 	memcpy(&ping->reply_pckt.icmp_pckt.icmphdr, buffer + IP_HDR_SIZE, ICMP_HDR_SIZE);
 	memcpy(ping->reply_pckt.icmp_pckt.data, buffer + IP_HDR_SIZE + ICMP_HDR_SIZE, ICMP_DATA_SIZE);
 
-	status = update_timestamps(&ping->stats);
-	if (status < 0)
-		clean_exit(ping->send_socket, ping->recv_socket, \
-			ping->stats.rtt_list, status * -1);
 	return bytes_received;
 }
 
@@ -32,6 +27,7 @@ ssize_t	receive_echo_reply(t_ping *ping)
 {
 	int		sig;
 	ssize_t	bytes_received;
+	int		status;
 
 	sig = 0;
 	bytes_received = -1;
@@ -46,9 +42,16 @@ ssize_t	receive_echo_reply(t_ping *ping)
 			break ;
 		if (check_packet_id(ping->reply_pckt.icmp_pckt.icmphdr, \
 				ping->icmp_pckt_request.icmphdr) == false)
+		{
 			bytes_received = -1;
-		else if (check_checksum_reply(&ping->reply_pckt.icmp_pckt))
+			continue ;
+		}
+		if (check_checksum_reply(&ping->reply_pckt.icmp_pckt))
 			return -1;
+		status = update_timestamps(&ping->stats);
+		if (status < 0)
+			clean_exit(ping->send_socket, ping->recv_socket, \
+				ping->stats.rtt_list, status * -1);
 		ping->stats.received_pckt++;
 	}
 	if (ping->reply_pckt.icmp_pckt.icmphdr.type == 3)
